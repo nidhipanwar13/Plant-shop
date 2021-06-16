e45bb2c3e0e97eb9e61ee9345cf0b21e0fc26ba1
Nidhi Panwar <pnwrnidhi@gmail.com>
2021-06-16 22:45:26 +0530

    header and home component added

--- src/Components/Maincomponent.js
+++ src/Components/Maincomponent.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Header from "./Headercomponent";
 import Footer from "./Footercomponent";
 import Home from "./Homecomponent";
+import Cacti from "./Cacticomponent";
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter as Router, Route, Switch, Redirect, withRouter } from 'react-router-dom';
@@ -16,17 +19,27 @@ class Main extends Component {
             <div>
             <Router>
                 <Header />
+                <Home/>
+               
 
+                <Switch >
+                <Route exact path="/Home" component={Home} />
+                {/* <Route exact path="/Cacti" component={Cacti} /> */}
+
+
+                    </Switch>
+
+{/* 
                         <Switch >
                             <Route exact path="/Home" component={Home} />
-                            {/* <Route exact path="/Bedroom" component={Bedroom} />
+                             <Route exact path="/Bedroom" component={Bedroom} />
                             <Route exact path="/Dining" component={Dining} />
                             <Route exact path="/Living" component={Living} />
                             <Route exact path="/Workplace" component={Workplace} />
-                            <Route exact path="/Contact" component={Contact} /> */}
+                            <Route exact path="/Contact" component={Contact} /> 
 
                             <Redirect to="/Home" />
-                        </Switch>
+                        </Switch> */}
                   
             </Router>
             <Footer />
